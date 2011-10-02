f6b050616df99aac85e7bb4e6ec5cfc6e2daee4c
robson junior <robson@kimmay.com.br>
2011-10-01 21:53:14 -0300

    refactoring and added new methods

--- spec/rgis_spec.rb
+++ spec/rgis_spec.rb
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-require 'spec_helper'
-=======
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
->>>>>>> 268b02855913b144c2956ca7dce22f75f6eda202
 
 describe "rgis module" do
 
