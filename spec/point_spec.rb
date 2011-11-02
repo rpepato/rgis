a40f1cc7943fab8048b3b369462c2cbacdcd1dbf
pepato <rpepato@gmail.com>
2011-11-02 12:54:17 -0200

    refactoring on point spec

--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -4,40 +4,37 @@ describe 'Point Geometry' do
 
   before (:each) do
     RGis::Services::ServiceDirectory.uri = "http://sampleserver1.arcgisonline.com/ArcGIS/rest/services"
+    @point = RGis::Point.new(-104.53, 34.74)
+    @projected_point = RGis::Point.new(-11636226.3726209, 4128604.07739995)
   end
 
   it "should points with equals [x,y] be compared as equals" do
-    RGis::Point.new(15,17).should == RGis::Point.new(15,17)
+    @point.should == RGis::Point.new(-104.53, 34.74)
   end
   
   it "should convert a point to a json representation expected by esri api" do
-    json = '{"geometryType":"esriGeometryPoint","geometries":[{"x":15.0,"y":17.0}]}'    
-    point = RGis::Point.new(15,17)
-    point.to_json.should == json    
+    json = '{"geometryType":"esriGeometryPoint","geometries":[{"x":-104.53,"y":34.74}]}'    
+    @point.to_json.should == json    
   end
 
   it "should project a point from one spatial reference to another and return a new point" do
     VCR.use_cassette('point_project', :record => :new_episodes) do
-      point = RGis::Point.new(15,17)
-      another_point = RGis::Point.new(1669792.3618991, 1920825.04037747)
-      point.project(:from => 4326, :to => 102100).should == another_point
-      another_point.project(:from => 102100, :to => 4326).should == point
+      @point.project(:from => 4326, :to => 102113).should == @projected_point
+      @projected_point.project(:from => 102113, :to => 4326).should == @point
     end
   end
   
   it "should change x and y atrributes from a point when reprojected" do
     VCR.use_cassette('point_project_bang', :record => :new_episodes) do
-      point = RGis::Point.new(15,17)
-      point.project!(:from => 4326, :to => 102100)
-      point.x.should == 1669792.3618991
-      point.y.should == 1920825.04037747
+      @point.project!(:from => 4326, :to => 102113)
+      @point.x.should == -11636226.3726209
+      @point.y.should == 4128604.07739995
     end
   end
   
   it "should simplify a point" do
     VCR.use_cassette('point_simplify', :record => :new_episodes) do
-      point = RGis::Point.new(15,17)
-      point.simplify(:spatial_reference => 4326).should == point
+      @point.simplify(:spatial_reference => 4326).should == @point
     end
   end
   
@@ -45,17 +42,17 @@ describe 'Point Geometry' do
     VCR.use_cassette('point_buffer', :record => :new_episodes) do
       point = RGis::Point.new(15,17)
       ring = RGis::Ring.new()
-      ring.points << RGis::Point.new(1669792.3618991, 1920825.04342547)
-      ring.points << RGis::Point.new(1669792.36453875, 1920825.04190147)
-      ring.points << RGis::Point.new(1669792.36453875, 1920825.03885346)
-      ring.points << RGis::Point.new(1669792.3618991, 1920825.03732946)
-      ring.points << RGis::Point.new(1669792.35925945, 1920825.03885346)
-      ring.points << RGis::Point.new(1669792.35925945, 1920825.04190147)
-      ring.points << RGis::Point.new(1669792.3618991, 1920825.04342547)
+      ring.points << RGis::Point.new(-11636226.3726209, 4128604.08044796)
+      ring.points << RGis::Point.new(-11636226.3699812, 4128604.07892395)
+      ring.points << RGis::Point.new(-11636226.3699812, 4128604.07587595)
+      ring.points << RGis::Point.new(-11636226.3726209, 4128604.07435194)
+      ring.points << RGis::Point.new(-11636226.3752605, 4128604.07587595)
+      ring.points << RGis::Point.new(-11636226.3752605, 4128604.07892395)
+      ring.points << RGis::Point.new(-11636226.3726209, 4128604.08044796)
       polygon = RGis::Polygon.new()
       polygon.rings << ring
 
-      point.buffer( :input_spatial_reference => 4326,
+      @point.buffer( :input_spatial_reference => 4326,
                     :output_spatial_reference => 102100,
                     :buffer_spatial_reference => 102100,
                     :distances => 0.01,
@@ -66,8 +63,7 @@ describe 'Point Geometry' do
   
   it "should raise an exception when area_and_perimeter method is called" do
     VCR.use_cassette('point_area_and_perimeter', :record => :new_episodes) do
-      point = RGis::Point.new(15,17)
-      lambda{point.area_and_perimeter(nil)}.should raise_error(TypeError, "Area and perimeter operation is allowed only for polygon type")
+      lambda{@point.area_and_perimeter(nil)}.should raise_error(TypeError, "Area and perimeter operation is allowed only for polygon type")
     end
   end
 
