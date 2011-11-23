6624b91deb27b17f866bd4f43bb2c52d8f66ae64
pepato <rpepato@gmail.com>
2011-11-23 17:44:18 -0200

    Refactoring on ring and path specs

--- spec/path_spec.rb
+++ spec/path_spec.rb
@@ -1,24 +1,25 @@
 require 'spec_helper'   
 
 describe 'Path Equality And Validation' do
+  before(:each) do
+    @path = RGis::Path.new()
+    @another_path = RGis::Path.new()
+    @path.points << RGis::Point.new(1,3) << RGis::Point.new(4,7) << ::RGis::Point.new(6,8)
+    @another_path.points << RGis::Point.new(1,3) << RGis::Point.new(4,7) << ::RGis::Point.new(6,8)
+  end
+  
   it "should compare equal paths" do
-    path = RGis::Path.new()
-    another_path = RGis::Path.new()
-    path.points << RGis::Point.new(1,3) << RGis::Point.new(4,7) << ::RGis::Point.new(6,8)
-    another_path.points << RGis::Point.new(1,3) << RGis::Point.new(4,7) << ::RGis::Point.new(6,8)
-    path.should == another_path
+    @path.should == @another_path
   end
   
   it "should validate a polyline when more than one point is present" do
-    path = RGis::Path.new()
-    path.points << RGis::Point.new(4,5) << RGis::Point.new(7,9)
-    path.should be_valid
+    @path.should be_valid
   end
   
   it "should reject a polyline when only a point is present" do
-    path = RGis::Path.new()
-    path.points << RGis::Point.new(4,6)
-    path.should_not be_valid
+    @path.points.pop
+    @path.points.pop
+    @path.should_not be_valid
   end
   
 end
--- spec/ring_spec.rb
+++ spec/ring_spec.rb
@@ -2,36 +2,35 @@ require 'spec_helper'
 
 describe 'Ring Equality and Validation' do
   
+  before(:each) do
+    @ring = RGis::Ring.new()
+    @another_ring = RGis::Ring.new()
+    @another_ring.points << RGis::Point.new(1,2) << RGis::Point.new(5,6) << RGis::Point.new(-14,6) << RGis::Point.new(1,2)    
+    @ring.points << RGis::Point.new(1,2) << RGis::Point.new(5,6) << RGis::Point.new(-14,6) << RGis::Point.new(1,2)
+  end
+  
   it "should compare equal rings" do
-    ring = RGis::Ring.new()
-    another_ring = RGis::Ring.new()
-    another_ring.points << RGis::Point.new(1,2) << RGis::Point.new(5,6) << RGis::Point.new(-14,6)    
-    ring.points << RGis::Point.new(1,2) << RGis::Point.new(5,6) << RGis::Point.new(-14,6)
-    ring.should == another_ring
+    @ring.should == @another_ring
   end
   
   it "should validate a ring with closed polygon" do
-    ring = RGis::Ring.new()
-    ring.points << RGis::Point.new(2,0) << RGis::Point.new(8,0) << RGis::Point.new(8,5) << RGis::Point.new(2,0)
-    ring.should be_valid
+    @ring.points << RGis::Point.new(1,2)
+    @ring.should be_valid
   end
   
   it "should reject a ring with less than 4 points" do
-    ring = RGis::Ring.new()
-    ring.points << RGis::Point.new(0,0) << RGis::Point.new(1,1) << RGis::Point.new(0,0)
-    ring.should_not be_valid
+    @ring.points.pop
+    @ring.should_not be_valid
   end
   
   it "should reject a ring with open polygon (path)" do
-    ring = RGis::Ring.new()
-    ring.points << RGis::Point.new(0,0) << RGis::Point.new(1,1) << RGis::Point.new(2,2) << RGis::Point.new(3,3)
-    ring.should_not be_valid    
+    @ring.points.pop
+    @ring.points << RGis::Point.new(1,1)
+    @ring.should_not be_valid    
   end
   
   it "should return raw data for ring" do
-    ring = RGis::Ring.new()
-    ring.points << RGis::Point.new(2,4) << RGis::Point.new(4,8)
-    ring.to_array.should =~ [[2,4],[4,8]]
+    @ring.to_array.should =~ [[1,2],[5,6],[-14,6],[1,2]]
   end
   
 end
